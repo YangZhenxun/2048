13ae942b6502d4f22ed96c670b7c95e4f403b302
YangZhenxun <2210067895@qq.com>
2024-09-16 18:20:07 +0800

    Init commit

---
+++ main_turtle.py
@@ -0,0 +1,8 @@
+import turtle
+import random
+#import pyside6 #It works as well but it uses Pyside6 and it is different from turtle(usage)...
+#PySide6 is strong, and it's simple to use, however: pip install pyside6 #first
+#And I don't know how to use it dynamically...
+#But Pyside6 is the option I recommend the most.
+sc: turtle._Screen = turtle.Screen()
+sc.setup()
